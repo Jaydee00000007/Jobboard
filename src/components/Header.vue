e6b27fb40251843d246a8b31d18e0d7fe9dc5edb
Opeyemi Adeparusi <118362022+Jaydee00000007@users.noreply.github.com>
2026-09-26 09:24:02 +0100

    refactor: improve accessible responsive header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,90 +1,108 @@
 <template>
-    <header class="blur">
-        <nav>
-            <div class="logo">
-                <img src="../images/logo.svg" alt="Logo" />
-                <h4>Job Hunt</h4>
-            </div>
-            <ul class="nav-link">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About us</a></li>
-                <li><a href="/jobs">Find jobs</a></li>
-                <li><a href="/faq">FAQ</a></li>
-            </ul>
-        </nav>
-    </header>
+  <header class="blur">
+    <nav aria-label="Primary navigation">
+      <RouterLink class="logo" to="/" aria-label="Job Hunt home">
+        <img src="../images/logo.svg" alt="" />
+        <span>Job Hunt</span>
+      </RouterLink>
+
+      <ul class="nav-link">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About us</RouterLink></li>
+        <li><RouterLink to="/jobs">Find jobs</RouterLink></li>
+        <li><RouterLink to="/faq">FAQ</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
-<script>
-export default {
-  name: 'Header'
-}
+<script setup lang="ts">
 </script>
 
 <style scoped>
-        nav{
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            gap: 5px;
-            padding: 0px 40px;
-            background-color: #071a29;
-            margin: 0px;
-            width: 100%;
-            height: 60px;
-            box-shadow: 0 4px 30px #071a29;
-        }
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  padding: 0 40px;
+  background-color: #071a29;
+  margin: 0;
+  width: 100%;
+  min-height: 60px;
+  box-shadow: 0 4px 30px #071a29;
+}
 
-        .blur{
-            background-color: #061724;
-            backdrop-filter: blur(10px);
-            box-shadow: 0 4px 30px #071a29;
-        }
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  min-height: 37px;
+  width: 150px;
+  background-color: #d2efff;
+  border-radius: 40px;
+  color: #071a29;
+  text-decoration: none;
+}
 
-        .logo img{
-            width: 40px;
-            height: auto;
-        }
+.logo img {
+  width: 40px;
+  height: auto;
+}
 
-        .logo{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 10px;
-            height: 37px;
-            width: 150px;
-            background-color: #d2efff;
-            border-radius: 40px;
-        }
+.logo span {
+  font-family: "Lato", sans-serif;
+  font-weight: 900;
+  font-size: 15px;
+}
 
-        .logo h4{
-            color: #071a29;
-            font-family: "Lato", sans-serif;
-            font-weight: 900;
-            font-style: normal;
-            font-size: 15px;
-        }
+.nav-link {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 70px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
 
-        .nav-link{
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            gap: 70px;
-            transition: 0px, 50px;
-        }
+.nav-link a {
+  text-decoration: none;
+  color: #d2efff;
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
+  font-size: 13px;
+}
 
-        .nav-link a, li{
-            text-decoration: none;
-            list-style: none;
-            color: #d2efff;
-            font-family: "Lato", sans-serif;
-            font-weight: 700;
-            font-style: normal;
-            font-size: 13px;
-        }
-        nav a:hover{
-            color: #36d2ff;
-            border-bottom: 2px solid orange;
-            transition: 0.4s ease-in-out;
-        }
+.nav-link a:hover,
+.nav-link a.router-link-active {
+  color: #36d2ff;
+}
+
+.nav-link a:focus-visible,
+.logo:focus-visible {
+  outline: 2px solid #36d2ff;
+  outline-offset: 4px;
+}
+
+@media (max-width: 700px) {
+  nav {
+    padding: 0 16px;
+  }
+
+  .nav-link {
+    gap: 18px;
+  }
+
+  .nav-link li:nth-child(2),
+  .nav-link li:nth-child(4) {
+    display: none;
+  }
+
+  .logo {
+    width: auto;
+    padding: 0 12px;
+  }
+}
 </style>
