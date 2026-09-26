cc4171cd8a349d0a2ae8d5cd7fd3cec7c70b753a
Opeyemi Adeparusi <118362022+Jaydee00000007@users.noreply.github.com>
2026-09-26 09:23:59 +0100

    refactor: migrate home view to typescript

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,45 @@
 <template>
   <div class="homepage">
-     <Header class="header"/>
-      <Main />
-      <Footer />
+    <Header class="header" />
+    <Main />
+    <Footer />
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import Header from '../components/Header.vue'
 import Main from '../components/Main.vue'
 import Footer from '../components/Footer.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    Header,
-    Main,
-    Footer
-  }
-}
 </script>
 
 <style>
-  body {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100vh;
-    overflow-x: hidden;
-    box-sizing: border-box;
-  }
-
-  .homepage {
-    min-height: 100dvh;
-    background-color: #071a29;
-  }
-
-  .blur{
-    background-color: rgba(255, 255, 255, 0.048);
-    backdrop-filter: blur(10px);
-    border: 1px solid rgba(255, 255, 255, 0);
-    box-shadow: 0 4px 30px rgba(19, 17, 17, 0.1);
-    border-radius: 12px;
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  min-height: 100vh;
+  overflow-x: hidden;
+  box-sizing: border-box;
 }
 
-.header{
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 1000;
+.homepage {
+  min-height: 100dvh;
+  background-color: #071a29;
+}
+
+.blur {
+  background-color: rgba(255, 255, 255, 0.048);
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0);
+  box-shadow: 0 4px 30px rgba(19, 17, 17, 0.1);
+  border-radius: 12px;
+}
+
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
 }
 </style>
