bd2bbb986ad46338f9082e11949d7efe4ee65eb4
Opeyemi Adeparusi <118362022+Jaydee00000007@users.noreply.github.com>
2026-09-26 08:44:26 +0100

    fix: configure eslint to parse vue typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import vue from 'eslint-plugin-vue'
 import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   {
@@ -14,12 +15,24 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node
+      },
+      parserOptions: {
+        parser: tsParser
       }
     },
     rules: {
       'vue/multi-word-component-names': 'off',
+      'vue/no-reserved-component-names': 'off',
       'vue/no-unused-vars': 'warn',
-      'no-unused-vars': 'warn'
+      'no-unused-vars': 'warn',
+      'vue/max-attributes-per-line': 'off',
+      'vue/html-indent': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/html-closing-bracket-spacing': 'off',
+      'vue/attributes-order': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/multiline-html-element-content-newline': 'off',
+      'vue/component-definition-name-casing': 'off'
     }
   }
 ]
