607c6549a69b0cb1601aee5f38e060eee049b123
Opeyemi Adeparusi <118362022+Jaydee00000007@users.noreply.github.com>
2026-09-26 09:24:15 +0100

    refactor: keep app shell focused on routing

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,3 @@
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/faq">FAQ</RouterLink>
-    <RouterLink to="/jobs">Find jobs</RouterLink>
-    <RouterLink to="/companies">Companies</RouterLink>
-  </nav>
   <RouterView />
 </template>
-
-<style>
-
-</style>
